2c11433b189a03895a4a7c263a20af524ab9b0db
Jheniel Guardiana <jhenielguardiana@Jheniels-MacBook-Pro.local>
2025-11-17 20:57:17 +0800

    Update Test Cases

--- tests/Admin/admin_uploadprescription.spec.ts
+++ tests/Admin/admin_uploadprescription.spec.ts
@@ -10,41 +10,40 @@ test.describe('Admin Upload Prescription', () => {
     await page.getByText('').click();
     await page.getByRole('button', { name: ' Rezept hochladen' }).click();
     await expect(page.locator('#root')).toContainText('VO Upload');
-    await expect(page.locator('#root')).toContainText('66-1111-1S. Zeibig13.11.2025Fehlende Upload-ID-2 notesView');
     await page.getByTestId('button').nth(1).click({force: true});
     await expect(page.getByTestId('modal-surface')).toContainText('Prescription Image Details');
     await page.getByRole('textbox', { name: 'Type your note here...' }).click();
     await page.getByRole('textbox', { name: 'Type your note here...' }).fill('automation admin test');
     await page.getByRole('button', { name: 'Geben Sie Ihre Notiz hier ein' }).click();
     await expect(page.getByTestId('modal-surface')).toContainText('automation admin test');
-    await page.getByRole('button', { name: 'Save Changes' }).click();
+    await page.getByText('Close').click();
    
     });
     
-    // test('Admin Upload Prescription Search', { tag: ['@Admin', '@SearchUploadVO'] }, async ({ page }) => {
-    // await page.getByText('').click();
-    // await page.getByRole('button', { name: ' Rezept hochladen' }).click();
-    // await expect(page.locator('#root')).toContainText('VO Upload');
-    // // open dropdown
-    // await page.getByRole('button', { name: 'Status' }).click();
-    // // click the option
-    // await page.getByRole('option', { name: 'In Prüfung (1)' }).click();
-    // // assert the dropdown now shows the selected value
-    // await expect(page.getByText('In Prüfung (1)')).toBeVisible();
-    // await expect(page.locator('#root')).toContainText('In Prüfung');
-    // await page.getByText('').click();
-    // await page.locator('div').filter({ hasText: /^Status$/ }).nth(2).click({force: true });
-    // await page.locator('div').filter({ hasText: 'All (5)In Prüfung (1)Nicht' }).nth(5).click();
-    // await page.getByText('Status', { exact: true }).first().click();
-    // await page.getByText('Angelegt (2)').click({force: true});
-    // await expect(page.locator('#root')).toContainText('Angelegt');
-    // await page.getByText('').click();
-    // await page.getByRole('textbox', { name: 'Suchen' }).click();
-    // await page.getByRole('textbox', { name: 'Suchen' }).fill('sandra');
-    // await page.getByRole('textbox', { name: 'Suchen' }).press('Enter');
-    // await expect(page.locator('#root')).toContainText('S. Zeibig');
+    test('Admin Upload Prescription Search', { tag: ['@Admin', '@SearchUploadVO'] }, async ({ page }) => {
+    await page.getByText('').click();
+    await page.getByRole('button', { name: ' Rezept hochladen' }).click();
+    await expect(page.locator('#root')).toContainText('VO Upload');
+    // open dropdown
+    await page.locator("div").filter({ hasText: "Status" }).click();
+    // click the option
+    await page.getByText('In Prüfung' ).click();
+    // assert the dropdown now shows the selected value
+    await expect(page.getByText('In Prüfung (1)')).toBeVisible();
+    await expect(page.locator('#root')).toContainText('In Prüfung');
+    await page.getByText('').click();
+    await page.locator('div').filter({ hasText: /^Status$/ }).nth(2).click({force: true });
+    await page.locator('div').filter({ hasText: 'All (5)In Prüfung (1)Nicht' }).nth(5).click();
+    await page.getByText('Status', { exact: true }).first().click();
+    await page.getByText('Angelegt (2)').click({force: true});
+    await expect(page.locator('#root')).toContainText('Angelegt');
+    await page.getByText('').click();
+    await page.getByRole('textbox', { name: 'Suchen' }).click();
+    await page.getByRole('textbox', { name: 'Suchen' }).fill('sandra');
+    await page.getByRole('textbox', { name: 'Suchen' }).press('Enter');
+    await expect(page.locator('#root')).toContainText('S. Zeibig');
 
-    // });
+    });
 
     // test('Admin Upload Prescription Update Status', { tag: ['@Admin', '@updateStatusUploadVO'] }, async ({ page }) => {
     // await page.getByText('').click();
---
+++ tests/Therapist/uploadprescription.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Therapist Upload Prescription', () => {
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://staging.therapios.de/therapist/'); // already logged in due to storageState
+  });
+
+  test('Therapist Upload VO', { tag: ['@Therapist','@uploadvo'] }, async ({ page }) => {
+    await page.getByText('').click();
+    await page.getByRole('button', { name: ' Rezept hochladen' }).click();
+    await expect(page.locator('#root')).toContainText('VO Upload');
+    await page.getByRole('button', { name: '󰩎 Rezept hochladen' }).click();
+    await page.getByRole('button', { name: 'Continue' }).click();
+    const [fileChooser] = await Promise.all([
+    page.waitForEvent("filechooser"),
+    page.getByText("Wählen Sie ein Bild zum Hochladen").click(),]);
+    await fileChooser.setFiles("/Users/jhenielguardiana/Documents/Therapois/tests/Therapist/sampleprescription.png");
+    await page.getByRole('button', { name: 'Submit' }).click();
+    await expect(page.locator('#root')).toContainText('In Prüfung');
+    });
+
+  test('Therapist Upload VO View and Add Note', { tag: ['@Therapist', '@AddNoteTherapist'] }, async ({ page }) => {
+    await page.getByText('').click();
+    await page.getByRole('button', { name: ' Rezept hochladen' }).click();
+    await expect(page.locator('#root')).toContainText('VO Upload');
+    await page.getByTestId('button').nth(1).click({force: true});
+    await expect(page.getByTestId('modal-surface')).toContainText('Prescription Image Details');
+    await page.getByRole('textbox', { name: 'Type your note here...' }).click();
+    await page.getByRole('textbox', { name: 'Type your note here...' }).fill('automation therapist test');
+    await page.getByRole('button', { name: 'Geben Sie Ihre Notiz hier ein' }).click();
+    await expect(page.getByTestId('modal-surface')).toContainText('automation therapist test');
+    await page.getByText('Close').click();
+    });
+
+});
